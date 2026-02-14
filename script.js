da54b13cba0f55d22645559a971322cb85a25462
jaswinder792 <singhmani2jpk@gmail.com>
2026-02-13 21:50:28 -0500

    then js code started with step 1 arrays for story parts to make sentence

---
+++ script.js
@@ -0,0 +1,10 @@
+/* ===========================
+   STEP 1:  ARRAYS FOR STORY PARTS TO MAKE SENTENCE
+   =========================== */
+
+
+const characters = ["The dragon", "A robot", "The princess", "A pirate", "The cat"];
+const actions = ["ate", "chased", "found", "lost", "built"];
+const places = ["in the park", "on the moon", "at school", "in the castle", "under the sea"];
+const times = ["yesterday", "last night", "this morning", "one day", "long ago"];
+const feelings = ["quietly.", "happily.", "angrily.", "excitedly.", "mysteriously."];

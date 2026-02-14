27ee911849ccab1e70177c3e545be71fea47d060
jaswinder792 <singhmani2jpk@gmail.com>
2026-02-13 21:51:06 -0500

    then contimued step 2 indexes added to  keep  the track of the current position in each array

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ const actions = ["ate", "chased", "found", "lost", "built"];
 const places = ["in the park", "on the moon", "at school", "in the castle", "under the sea"];
 const times = ["yesterday", "last night", "this morning", "one day", "long ago"];
 const feelings = ["quietly.", "happily.", "angrily.", "excitedly.", "mysteriously."];
+// step 2: indexes added to  keep  the track of the current position in each array
+let index1 = 0;
+let index2 = 0;
+let index3 = 0;
+let index4 = 0;
+let index5 = 0;

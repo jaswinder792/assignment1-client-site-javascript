517af1d5f96556994b04df1039c079c337cfd16d
jaswinder792 <singhmani2jpk@gmail.com>
2026-02-13 21:54:06 -0500

    step 5  aded button scycling functinonality to cycle through each button*/ btn1.onclick = function () {

--- script.js
+++ script.js
@@ -25,3 +25,32 @@ const btn4 = document.getElementById("btn4");
 const btn5 = document.getElementById("btn5");
 
 const output = document.getElementById("output");
+ /* step 5  aded button scycling functinonality to cycle through each button*/ 
+btn1.onclick = function () {
+    selected[0] = characters[index1];
+    btn1.textContent = selected[0];
+    index1 = (index1 + 1) % characters.length;
+};
+
+btn2.onclick = function () {
+    selected[1] = actions[index2];
+    btn2.textContent = selected[1];
+    index2 = (index2 + 1) % actions.length;
+};
+btn3.onclick = function () {
+    selected[2] = places[index3];
+    btn3.textContent = selected[2];
+    index3 = (index3 + 1) % places.length;
+};
+
+btn4.onclick = function () {
+    selected[3] = times[index4];
+    btn4.textContent = selected[3];
+    index4 = (index4 + 1) % times.length;
+};
+
+btn5.onclick = function () {
+    selected[4] = feelings[index5];
+    btn5.textContent = selected[4];
+    index5 = (index5 + 1) % feelings.length;
+};
